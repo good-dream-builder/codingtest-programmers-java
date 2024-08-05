08f1752884673dd17063d9a789864f6de30b0b9f
good.dream.builder <good.dream.builder@gmail.com>
2024-08-06 00:43:37 +0900

    12946 셋업.

---
+++ src/com/songko/Solution_12946.java
@@ -0,0 +1,27 @@
+package com.songko;
+
+import com.songko.common.Log;
+import com.songko.common.TestInterface;
+
+/**
+ * 하노이의 탑
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12946
+ *
+ * 시간 분, 회차 통과
+ */
+public class Solution_12946 implements TestInterface {
+    public int[][] solution(int n) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void test() {
+
+    }
+}
