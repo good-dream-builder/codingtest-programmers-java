223306870da0cb42263b5c33ca46dd40bfd77dca
psj <good.dream.builder@gmail.com>
2024-08-09 11:32:51 +0900

    84512 완료

--- src/com/songko/Solution_84512.java
+++ src/com/songko/Solution_84512.java
@@ -7,60 +7,40 @@ import com.songko.common.TestInterface;
  * 모음사전
  * https://school.programmers.co.kr/learn/courses/30/lessons/84512
  * <p>
- * 16:55 ~
+ * 2시간 4분, 1회차 통과
  */
 public class Solution_84512 implements TestInterface {
 
-    class Given {
-        int lastIdx;
-        String[] words = new String[]{"A", "E", "I", "O", "U"};
+    Integer step = 0;
+    final String[] given = new String[]{"A", "E", "I", "O", "U"};
 
-        public String getCurrentWord() {
-            return words[lastIdx];
+    public boolean find(String search, String word) {
+        if (search.equals(word)) {
+            return true;
         }
 
-        public String getNextWord(int index) {
-            int pivot = index / 5;
-            if (pivot > 0 && index % 5 == pivot) {
-                lastIdx++;
+        if (step > 0 && word.length() % 5 == 0) {
+            return false;
+        }
+
+        boolean isFind = false;
+        for (int i = 0; i < 5; i++) {
+            step++;
+            isFind = find(search, word.concat(given[i]));
+
+            if (isFind) {
+                break;
             }
-            return words[lastIdx % 5];
-        }
-    }
 
-
-    public void find(String search, StringBuffer word, int index, Given given) {
-        if (word.toString().equals(search)) {
-            Log.print(index + " : " + word.toString());
-            return;
+            word = word.substring(0, word.length());
         }
 
-        int len = word.length();
-        if (len > 2) {
-            if (word.substring(len - 2, len - 1).equals("U")) {
-                return;
-            }
-        }
-
-        if (len == 5) {
-            word = new StringBuffer(word.substring(0, 4));
-            word.append(given.getNextWord(index));
-            find(search, word, index + 1, given);
-            return;
-        }
-
-
-
-        word.append(given.getNextWord(index));
-        Log.print(index + " : " + word.toString());
-
-        find(search, word, index + 1, given);
+        return isFind;
     }
 
     public int solution(String word) {
-        int answer = 0;
-        find(word, new StringBuffer(), 0, new Given());
-
+        find(word, "");
+        int answer = step;
         return answer;
     }
 
@@ -71,20 +51,24 @@ public class Solution_84512 implements TestInterface {
 
     @Override
     public void test() {
+//        // 6
 //        String tc1 = "AAAAE";
 //        int solv1 = solution(tc1);
 //        Log.print(solv1);
 //
-        String tc2 = "AAAE";
-        int solv2 = solution(tc2);
-        Log.print(solv2);
-//
+//        // 10
+//        String tc2 = "AAAE";
+//        int solv2 = solution(tc2);
+//        Log.print(solv2);
+
+        // 1563
 //        String tc3 = "I";
 //        int solv3 = solution(tc3);
 //        Log.print(solv3);
-//
-//        String tc4 = "EIO";
-//        int solv4 = solution(tc4);
-//        Log.print(solv4);
+
+        // 1189
+        String tc4 = "EIO";
+        int solv4 = solution(tc4);
+        Log.print(solv4);
     }
 }
